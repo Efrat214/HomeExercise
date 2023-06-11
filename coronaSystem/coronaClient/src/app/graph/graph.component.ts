3838eee36bb582b8a5ebf8e7d87a3e86813827b7
Efrat214 <125562929+Efrat214@users.noreply.github.com>
2023-06-11 16:39:09 +0300

    Update graph.component.ts

--- coronaSystem/coronaClient/src/app/graph/graph.component.ts
+++ coronaSystem/coronaClient/src/app/graph/graph.component.ts
@@ -84,28 +84,4 @@ export class GraphComponent {
     }
     return new Blob([ab], { type: type });
   }
-  
-  
-  // createChart(){  
-  //   this.chart = new Chart("MyChart", {
-  //     type: 'line', //this denotes tha type of chart
-
-  //     data: {// values on X-Axis
-  //       labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-	// 							 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-	//        datasets: [
-  //         {
-  //           label: "active seeks",
-  //           data: ['467','576', '572', '79', '92',
-	// 							 '574', '573', '576'],
-  //           backgroundColor: 'lightblue'
-  //         }
-  //       ]
-  //     },
-  //     options: {
-  //       aspectRatio:2.5
-  //     }
-      
-  //   });
-  // }
 }
